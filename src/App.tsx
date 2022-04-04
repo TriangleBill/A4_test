65dc2c2d384f1baae9b5f243f372724e59278152
Triangle_Bill <d1demond1@gmail.com>
2022-04-04 15:55:40 +0300

    Create Profile page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Main from './components/Main';
 import NewsPage from './components/NewsPage';
+import ProfilePage from './components/ProfilePage';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
       <Route path='/news' element={<NewsPage />} />
+      <Route path='/profile' element={<ProfilePage />} />
     </Routes>
   );
 }
