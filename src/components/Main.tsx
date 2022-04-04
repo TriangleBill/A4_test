0e7b2e58a3810a26c6abeb0a476ff9c71e6e8f15
Triangle_Bill <d1demond1@gmail.com>
2022-04-04 15:38:38 +0300

    Create Home page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,21 @@ export default function Main(): JSX.Element {
         <>
             <Navbar />
             <div className='container'>
-                Main
+                <div className="main-page">
+                    <h1 className='title main-page__title'>Home Page</h1>
+
+                    <p className="main-page__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pharetra justo vel pulvinar. Duis quis elementum enim. Mauris fringilla volutpat pellentesque. Ut malesuada mattis dui. Sed gravida pharetra lectus, vel facilisis magna fermentum nec. Aliquam aliquam dapibus purus, eget molestie justo. Praesent pellentesque sollicitudin tortor, vitae elementum dolor euismod sed. Curabitur varius lectus in lobortis pellentesque. Mauris lectus felis, porta id hendrerit at, vestibulum non turpis. Integer sagittis, quam a consequat egestas, tellus magna imperdiet augue, nec euismod diam eros et sapien.
+
+                        Praesent ullamcorper lacinia purus, eget iaculis nisi cursus eu. Quisque faucibus at metus vitae rhoncus. Nam a sapien id felis volutpat gravida. Nulla tempus dui non augue vulputate interdum. Donec volutpat elit congue mattis porta. Phasellus magna velit, iaculis eget ante eget, egestas semper lacus. Sed nisi lectus, lobortis sed tempor feugiat, commodo id sapien. Pellentesque sodales odio purus, eu consequat elit bibendum eu. Etiam accumsan enim at ornare condimentum. Cras fermentum lacus sem, non pellentesque mauris blandit sollicitudin. Nam malesuada leo a leo varius, non egestas dui interdum. Phasellus imperdiet diam fermentum felis placerat, sit amet dictum dolor mattis. Sed quis neque id augue pretium sollicitudin eget et risus. Mauris euismod purus vitae enim dignissim vehicula. Nulla elementum a velit ac semper.
+
+                        Donec aliquam non nunc eget fermentum. Curabitur nec erat ac leo placerat dictum. Mauris ut lacus imperdiet, volutpat felis nec, consectetur lectus. Vestibulum fringilla, purus et lacinia euismod, ligula lorem varius nibh, eu vulputate odio metus vel felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam orci magna, laoreet nec tempus sit amet, elementum vel ante. Aliquam dapibus in nisl rutrum aliquam. Nam scelerisque facilisis nisl, ut fringilla augue maximus non.
+
+                        Nam vehicula condimentum nisl. Donec eget ipsum sed ipsum ullamcorper vulputate. Mauris sit amet eros id est commodo iaculis. Etiam venenatis maximus lorem placerat consequat. Morbi vulputate nisi dolor, non efficitur ligula dictum eget. Ut placerat massa et tristique malesuada. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ut nibh ut massa tempus feugiat quis eu augue.
+
+                        Ut ac purus vel metus tincidunt feugiat. Mauris cursus orci at elementum eleifend. Etiam at lobortis elit. In hac habitasse platea dictumst. In pharetra tincidunt neque, at venenatis felis auctor vel. Phasellus tempus augue accumsan, tincidunt purus in, congue augue. Praesent turpis nibh, hendrerit non auctor sit amet, placerat eu enim. Ut ornare egestas semper. Nunc pellentesque, metus in convallis pretium, nisi nulla mollis leo, a congue est nisi quis orci. In molestie ante quis tortor commodo viverra. Nullam ac neque aliquam, dignissim velit nec, vehicula tellus. Mauris vitae dapibus dui, sit amet molestie mauris. Quisque semper, diam non tincidunt luctus, orci leo rhoncus leo, non sagittis elit sapien nec arcu. Praesent rhoncus lacus sit amet sapien vehicula, id gravida ante gravida. Fusce eget eleifend magna, in volutpat erat. Aenean sit amet ligula felis.
+                    </p>
+                </div>
             </div>
         </>
 
