ab77718fda51a51697aa20d895b70997ca59d6a8
Triangle_Bill <d1demond1@gmail.com>
2022-04-04 14:42:40 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Main from './components/Main';
 
 function App(): JSX.Element {
   return (
-    <></>
+    <Routes>
+      <Route path='/' element={<Main />} />
+    </Routes>
   );
 }
 
